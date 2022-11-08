851e1db71b4879dc89c82cd2b09484df1bc52de7
alfanaziz <117435247+alfanaziz@users.noreply.github.com>
2022-11-08 14:54:02 +0700

    Add files via upload

---
+++ Scripts/PindahScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PindahScene : MonoBehaviour
+{
+    public string namaScene;
+
+    public void PindahkeScene()
+    {
+        Scene sceneini = SceneManager.GetActiveScene();
+
+        if (sceneini.name != namaScene)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+
+}
